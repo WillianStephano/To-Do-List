89cf6c9c9f9e21fc9839716cb29e932c8f611000
Willian Stephano <will.stephano@gmail.com>
2022-05-23 12:12:41 -0300

    Crianção de funcionalidade de criação de tarefa

--- JS/main.js
+++ JS/main.js
@@ -3,30 +3,57 @@ const btNovaTarefa = document.querySelector('.bt_nova_tarefa')
 btNovaTarefa.addEventListener( 'click', function (event) {
    event.preventDefault()
    
+   tagLI = criaNovaTarefa()
    
-   tagLI = CriaItemEmLista()
-   
-   var inputCheckbox = document.createElement('input')
-   inputCheckbox.setAttribute('type', 'checkbox')
-   tagLI.appendChild(inputCheckbox)
-   
-   
+   criaBtClose()
    
    
 })
 
-function CriaItemEmLista(){
-   var valorNovaTarefa = document.querySelector('.nova_tarefa_campo').value
-   
-   var tarefasLista = document.querySelector('.lista_tarefas')
 
-   var tagLI = document.createElement('li')
+window.onload = (event) =>{   
+   criaBtClose()
+}
+
+
+
+
+function criaNovaTarefa(){
+   let valorNovaTarefa = document.querySelector('.nova_tarefa_campo').value
+   let tarefasLista = document.querySelector('.lista_tarefas')
+   
+   let tagLI = document.createElement('li')
    tagLI.textContent = valorNovaTarefa
    tagLI.classList.add('item_tarefas')
    tarefasLista.appendChild(tagLI)
+   
 
    return tagLI
 }
+
+
+
+
+function criaBtClose() {
+   var tarefasLista = document.querySelectorAll('.item_tarefas')
+   
+   for (let i = 0; i < tarefasLista.length; i++) {
+      var elemento = tarefasLista[i]
+      var filho = elemento.childElementCount
+
+      if (filho == 0) {
+         let span = document.createElement('span')
+         span.classList.add('bt_fechar_item')
+         span.textContent = '\u00D7'
+         elemento.appendChild(span)
+      }
+   }
+   
+   
+   
+}
+
+
 
 
 
