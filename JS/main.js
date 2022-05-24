1e3b8e0bd5908b9ce03213951143a8d8806aac39
Willian Stephano <will.stephano@gmail.com>
2022-05-24 13:55:45 -0300

    Correção de erro em funcionalidade de excluir tarefa e criação de interatividade que limpa campo de texto

--- JS/main.js
+++ JS/main.js
@@ -1,63 +1,80 @@
-const btNovaTarefa = document.querySelector('.bt_nova_tarefa')
+const btNovaTarefa = document.querySelector(".bt_nova_tarefa");
 
-btNovaTarefa.addEventListener( 'click', function (event) {
-   event.preventDefault()
+btNovaTarefa.addEventListener("click", function (event) {
+   event.preventDefault();
    
-   tagLI = criaNovaTarefa()
+   tagLI = criaNovaTarefa();
    
-   criaBtClose()   
-   selecionaBtFechar()
+   criaBtClose();
+   selecionaBtFechar();
+   limparForm();
    
+});
+
+window.onload = (event) => {
+   criaBtClose();
+   selecionaBtFechar();
    
-})
+};
 
 
-window.onload = (event) =>{   
-   criaBtClose()
-   selecionaBtFechar()
+function limparForm() {
+   const formulario = document.querySelector(".formulario");
+   console.log(formulario);
+   formulario.reset()
+   
 }
 
 
-
-
-function criaNovaTarefa(){
-   let valorNovaTarefa = document.querySelector('.nova_tarefa_campo').value
-   let tarefasLista = document.querySelector('.lista_tarefas')
+function criaNovaTarefa() {
+   let valorNovaTarefa = document.querySelector(".nova_tarefa_campo").value;
+   let tarefasLista = document.querySelector(".lista_tarefas");
    
-   let tagLI = document.createElement('li')
-   tagLI.textContent = valorNovaTarefa
-   tagLI.classList.add('item_tarefas')
-   tarefasLista.appendChild(tagLI)
+   let tagLI = document.createElement("li");
+   tagLI.textContent = valorNovaTarefa;
+   tagLI.classList.add("item_tarefas");
+   tarefasLista.appendChild(tagLI);
    
+   return tagLI;
    
-   return tagLI
 }
-
-
 
 
 function criaBtClose() {
-   var tarefasLista = document.querySelectorAll('.item_tarefas')
+   var tarefasLista = document.querySelectorAll(".item_tarefas");
    
    for (let i = 0; i < tarefasLista.length; i++) {
-      var elemento = tarefasLista[i]
-      var filho = elemento.childElementCount
+      var elemento = tarefasLista[i];
+      var filho = elemento.childElementCount;
       
       if (filho == 0) {
-         let span = document.createElement('span')
-         span.classList.add('bt_fechar_item')
-         span.textContent = '\u00D7'
-         elemento.appendChild(span)
+         let span = document.createElement("span");
+         span.classList.add("bt_fechar_item");
+         span.textContent = "\u00D7";
+         elemento.appendChild(span);
       }
-   }  
-}
-
-function selecionaBtFechar() {
-   var btFecharTarefa = document.querySelectorAll('.bt_fechar_item')
-   for (let i = 0; i < btFecharTarefa.length; i++) {
-      btFecharTarefa[i].addEventListener('click', function (){
-         let tarefa = btFecharTarefa[i].parentNode
-         tarefa.classList.add('fechado')
-      })
    }
 }
+
+
+function selecionaBtFechar() {
+   var btFecharTarefa = document.querySelectorAll(".bt_fechar_item");
+   
+   for (let i = 0; i < btFecharTarefa.length; i++) {
+      btFecharTarefa[i].addEventListener("click", function () {
+         let tarefa = btFecharTarefa[i].parentNode;
+         tarefa.classList.add("fechado");
+      });
+      
+   }
+}
+
+
+var list = document.querySelector("ul");
+list.addEventListener("click" ,function (ev) {
+   if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+   }
+},
+false
+);
